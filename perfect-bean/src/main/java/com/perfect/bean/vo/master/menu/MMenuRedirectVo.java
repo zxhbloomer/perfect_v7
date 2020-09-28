c86bdb8d76f331bddfd0821c6e35b73aa79de1ef
zxhbloomer@gmail.com <Lyyzxh030324>
2020-09-28 16:07:32 +0800

    ..

---
+++ perfect-bean/src/main/java/com/perfect/bean/vo/master/menu/MMenuRedirectVo.java
@@ -0,0 +1,47 @@
+package com.perfect.bean.vo.master.menu;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 菜单重定向
+ * </p>
+ *
+ * @author zxh
+ * @since 2020-09-28
+ */
+@Data
+@NoArgsConstructor
+@ApiModel(value = "菜单重定向bean", description = "菜单重定向bean")
+@EqualsAndHashCode(callSuper=false)
+public class MMenuRedirectVo implements Serializable {
+
+    private static final long serialVersionUID = -7668181069536025626L;
+
+    private Long id;
+
+    /**
+     * 根结点id
+     */
+    private Long root_id;
+
+    /**
+     * 页面id
+     */
+    private Long page_id;
+
+    /**
+     * 菜单中，该页面的id
+     */
+    private Long menu_page_id;
+}
