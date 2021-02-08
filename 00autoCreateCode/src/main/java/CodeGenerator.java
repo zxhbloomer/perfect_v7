b1dcdb637928eed8687b7d98a5950af4a4589093
zxhbloomer@gmail.com <Lyyzxh030324>
2021-02-08 13:46:38 +0800

    ..

--- 00autoCreateCode/src/main/java/CodeGenerator.java
+++ 00autoCreateCode/src/main/java/CodeGenerator.java
@@ -100,7 +100,8 @@ public class CodeGenerator {
 //        strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
         //需要包含的表名，允许正则表达式
         strategy.setInclude(
-                "m_menu_redirect"
+                "m_user_default_menu",
+                "m_user_permission"
 
                 //,
 //                "oauth_access_token",
